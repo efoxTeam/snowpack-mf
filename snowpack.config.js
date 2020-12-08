e36b3e8eb816b6939899b10c069248c9f31718d9
xuhongbin <xuhongbin@yy.com>
2020-12-08 15:44:58 +0800

    feat: v0.0

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,18 @@
+const WebpackConfigFN = require('./webpack.config')
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
     /* ... */
+    public:'/',
+    src:'/_dist_'
   },
   plugins: [
     /* ... */
+    '@snowpack/plugin-react-refresh',
+    ['@snowpack/plugin-webpack',{
+      extendConfig: WebpackConfigFN
+    }],
+    ['@snowpack/plugin-dotenv',{}]
   ],
   install: [
     /* ... */
---
+++ src/ANTD.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button, DatePicker } from 'antd'
+const ANTD = () => (
+  <>
+    <Button type="primary">PRESS ME</Button>
+    <DatePicker placeholder="select date" />
+  </>
+);
+export default ANTD
